185e7f7a41598b16ba01362664c4004cc94ece37
AkiakiShibashiba <akiakira_ta_bashi_yookan@yahoo.co.jp>
2018-01-27 11:09:35 +0900

    create socket_test.cs which cannot keeping connect to server yet

---
+++ Assets/Scripts/socket_test.cs
@@ -0,0 +1,55 @@
+﻿// https://qiita.com/oishihiroaki/items/bb2977c72052f5dd5bd9
+
+using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+
+public class socket_test: MonoBehaviour {
+
+	WebSocket ws;
+
+	void Start()
+	{
+		ws = new WebSocket("ws://localhost:3000/");
+
+		ws.OnOpen += (sender, e) =>
+		{
+			Debug.Log("WebSocket Open");
+		};
+
+		ws.OnMessage += (sender, e) =>
+		{
+			Debug.Log(e.Data);
+		};
+
+		ws.OnError += (sender, e) =>
+		{
+			Debug.Log("WebSocket Error Message: " + e.Message);
+		};
+
+		ws.OnClose += (sender, e) =>
+		{
+			Debug.Log("WebSocket Close");
+		};
+
+		ws.Connect();
+
+	}
+
+	void Update()
+	{
+
+		if (Input.GetKeyUp("s"))
+		{
+			ws.Send("message");
+		}
+
+	}
+
+	void OnDestroy()
+	{
+		ws.Close();
+		ws = null;
+	}
+}
