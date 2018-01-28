764a50b4de65846710fc4e9f7bb0355a664b4ace
Tomoaki-Tsuchiya <tomt0m.25.g00cl@gmail.com>
2018-01-28 21:32:48 +0900

    Successfull Login and SceneChange

--- Assets/Scripts/LoginManager.cs
+++ Assets/Scripts/LoginManager.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+
+
+
 
 public class LoginManager : MonoBehaviour {
+	[System.Serializable]
+	public class LoginData{
+		public string Name, Message;
+	}
+
 	//private GameObject guiTextLogin;
 	private GameObject loginFailureText;
 
 	private bool loginFailure;
 	private bool loginButton;
-	public string id;
-	public string pw;
+	private string id = "";
+	private string pw = "";
+	private WebSocket ws;
+	private string person;
+	private bool login = false;
 
 
 	// Use this for initialization
@@ -23,44 +36,87 @@ public class LoginManager : MonoBehaviour {
 
 		//初めログインボタンは押されていない状態
 		loginButton = false;
-		//成功した時だけ失敗メッセージを消す
-		loginFailure = true;
+		//初めは常に成功
+		loginFailure = false;
 
+		ws = new WebSocket("ws://localhost:8080/ws");
+		//ws = new WebSocket("ws://billboard-wsserver.herokuapp.com/ws");
+		ws.OnOpen += (sender, e) =>
+		{
+			Debug.Log("WebSocket Open");
+		};
+
+		ws.OnMessage += (sender, e) =>
+		{
+			Debug.Log(e.Data);
+			LoginData recieve_message = new LoginData ();
+			recieve_message = JsonUtility.FromJson<LoginData>(e.Data);
+			if(recieve_message.Name =="tomo" && recieve_message.Message == "tomo"){
+				Debug.Log("Login:succeeded");
+				login = true;
+			}
+			else {
+				loginFailure = true;
+			}
+			person = recieve_message.Name;
+
+		};
+
+		ws.OnError += (sender, e) =>
+		{
+			Debug.Log("WebSocket Error Message: " + e.Message);
+		};
+
+		ws.OnClose += (sender, e) =>
+		{
+			Debug.Log("WebSocket Close");
+		};
+
+		ws.Connect();
+	}
+
+	void Update() {
+		if (login) {
+			SceneManager.LoadScene ("test",LoadSceneMode.Single);
+		}
 	}
 
 	void OnGUI(){//再生中GUIをずっと見張るメソッド
-
-		loginFailureText.SetActive (false);
-
+		if (loginFailure == false) {
+			loginFailureText.SetActive (false);
+		} else {
+			loginFailureText.SetActive (true);
+		}
 		drawLoginMenu ();
 
 		//ログインボタンが押された時の処理
 		if (loginButton) {
 			//ここでユーザ情報をサーバとやりとりwith socket
+			LoginData data = new LoginData ();
+			data.Name = id;
+			data.Message = pw;
+			string send_message = JsonUtility.ToJson (data);
+			Debug.Log (send_message);
 
+			ws.Send (send_message);
 
-			if (loginFailure == false) {
-				//ログイン成功。scene切り替える
-				SceneManager.LoadScene ("test");
-			} else {
+			if(loginFailure){
 				loginFailureText.SetActive (true);
 			}
-
 		}
-
 	}
 
 	void drawLoginMenu(){
 		// テキストボックスの設置と入力値の取得
 		GUI.skin.textField.fontSize = 20;
 		int txtW = 150, txtH = 40;
-		id = GUI.TextField     (new Rect(Screen.width*1/2, Screen.height*1/3 - txtH*1/2, txtW, txtH), id);
-		pw = GUI.PasswordField (new Rect(Screen.width*1/2, Screen.height*1/2 - txtH*1/2, txtW, txtH), pw, '*');
+		id = GUI.TextField     (new Rect(Screen.width*1/2 - txtW* 1/8, Screen.height*1/3 + txtH, txtW, txtH), id);
+		pw = GUI.PasswordField (new Rect(Screen.width*1/2 - txtW * 1/8, Screen.height*1/2 + txtH, txtW, txtH), pw, '*');
 
 		// ボタンの設置
 		int btnW = 180, btnH = 50;
 		GUI.skin.button.fontSize = 20;
-		loginButton      = GUI.Button( new Rect(Screen.width*1/4 - btnW*1/2, Screen.height*3/4 - btnH*1/2, btnW, btnH), "Log In" );
+		loginButton      = GUI.Button( new Rect(Screen.width*1/2 - btnW*1/2, Screen.height*3/4 + btnH*1/3, btnW, btnH), "Log In" );
 	}
 
 }
